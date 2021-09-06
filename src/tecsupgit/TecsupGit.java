a7b5a44e9c5657334e46f2980babd67f0bcc54b4
Jaibey Pisco <jaibey.pisco@tecsup.edu.pe>
2021-09-06 11:18:22 -0500

    agregamos el estudinte

--- src/tecsupgit/TecsupGit.java
+++ src/tecsupgit/TecsupGit.java
@@ -3,7 +3,8 @@ package tecsupgit;
 public class TecsupGit {
 	public static void main(String[] args) {
 		System.out.println("Hola estudiantes");
-		String studiante = "Jaibey Pisco";
+		String estudiante = "Jaibey Pisco";
+		System.out.println(estudiante);
 	}
 	
 
